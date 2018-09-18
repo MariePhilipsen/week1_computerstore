44beeebd76905d6bded60931f10e7af702e0de1b
Marie Philipsen <marie_philipsen@hotmail.com>
2018-09-18 15:21:32 +0200

    klasse HardDisk en ComputerSystem toegevoegd. In klasse ComputerSystem methodes addPeripherals en getNumberOfPheripherals(). Exception TooManyPheripherals aangemaakt en deze opgegooid in de klasse ComputerSystem.

---
+++ src/be/pxl/computerstore/hardware/HardDisk.java
@@ -0,0 +1,9 @@
+package be.pxl.computerstore.hardware;
+
+public class HardDisk extends ComputerComponent {
+    private int capacity;
+
+    public HardDisk(String vendor, String name, double price) {
+        super(vendor, name, price);
+    }
+}
